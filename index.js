74423133db8f21fc605b5f12c94bce699bd3f554
Sakshi08429 <chaudharysakshi5362@gmail.com>
2025-03-29 12:54:10 +0530

    handle CORS using proxies

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //exporting express 
 //it loads express module
+require('dotenv').config()
 const express=require('express')
 
 //app represents express server
 const app=express()
 
-const port=3000
+const port=process.env.PORT||4000
 //handling get request
 // "/"----->home route
 app.get('/',(req,res)=>{
@@ -17,7 +18,11 @@ app.get('/twitter',(req,res)=>{
     res.send('Hello,Twitter ....');
 })
 //starts the server and makes it listen to incoming requests
-app.listen(port,()=>{
-console.log('Server is running on port 3000')
-});
+// app.listen(port,()=>{
+// console.log('Server is running on port 4000')
+// });
 
+//from env variable
+app.listen(port,()=>{
+console.log(`Server is running on port  ${port}`)
+});
